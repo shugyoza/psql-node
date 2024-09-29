fa3bda526ba2a550ee279ec187f7016a133b1735
Stephen Hanjaya <stephenhanjaya@gmail.com>
2024-09-28 19:21:31 -0500

    update table_account_insert .sql
    Signed-off-by: Stephen Hanjaya <stephenhanjaya@gmail.com>

--- db/sql/table_account-insert.sql
+++ db/sql/table_account-insert.sql
@@ -12,19 +12,10 @@ VALUES
     'username_1',
     'password_1',
     'email_1',
-    'value_1',
+    '+0(000)000-0000',
     CURRENT_TIMESTAMP,
     CURRENT_TIMESTAMP
-  ),
-  (
-    'username_2',
-    'password_2',
-    'email_2',
-    'value_2',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-  ),
-  RETURNING user_id,
+  ) RETURNING user_id,
   username,
   email,
   phone;
